39321571ad2f19aa642aa09f2838e439fb655ec0
waynejones7 <130200849+waynejones7@users.noreply.github.com>
2024-08-21 15:12:06 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -225,8 +225,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="JA Chat"
+        subTitle="JA AI assistant."
         logo={<ChatGptIcon />}
       >
         <div className={styles["sidebar-header-bar"]}>
